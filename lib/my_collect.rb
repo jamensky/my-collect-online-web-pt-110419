071cf6cfad0739bd27c3ae486679ad2b7587c87a
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-18 00:30:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(languages)
   i = 0 
   new_elements = []
   while i < languages.length
-  new_elements << yield (languages[i].upcase)
+  new_elements << yield(languages[i].upcase)
   i+= 1
 end 
 return new_elements
