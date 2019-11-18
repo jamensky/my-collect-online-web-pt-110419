422340580e69bebea26b6f2abbae514b02cdc453
Jamal Rjoul <jamensky1991@gmail.com>
2019-11-18 00:28:37 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(languages)
+  i = 0 
+  new_elements = []
+  while i < languages.length
+  new_elements << yield languages[i].upcase
+  i+= 1
+end 
+return new_elements
+end
 
 
+=begin
+my_collect(['ruby', 'javascript', 'python', 'objective-c']) do 
+  {|language| puts language}
+end 
+
+=end
